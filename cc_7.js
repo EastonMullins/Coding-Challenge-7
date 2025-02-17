b98ef4ca6f445915a0699b79a6f434a269032c0f
Easton <eastonmullins@usf.edu>
2025-02-16 19:10:15 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,13 @@ function calculateInvoice(subtotal, taxRate, discount){
 }
 calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
+
+
+//Task 2 - Employee Hourly Wage Calculation
+
+function calculateHourlyWage(salary, hoursPerWeek){
+    let hourlyWage = (salary/ (hoursPerWeek * 52));
+    console.log(`Hourly Wage: $${hourlyWage}`);
+}
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
