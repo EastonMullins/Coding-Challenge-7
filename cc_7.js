f08d2e28c13cfe80df50338f0de51079245a10b3
Easton <eastonmullins@usf.edu>
2025-02-16 19:12:36 -0500

    Task 5 - Business Loan Interest Calculator

--- cc_7.js
+++ cc_7.js
@@ -52,3 +52,12 @@ function calculateShippingCost(weight, location, expedited = fasle){
 }
 calculateShippingCost(10, "USA", true); // Expected output: "Shipping Cost: $20.00"
 calculateShippingCost(5, "Canada", false); // Expected output: "Shipping Cost: $13.50"
+
+//Task 5 - Business Loan Interest Calculator
+
+function calculateLoanInterest(principal, rate, years){
+    let Interest = (principal * rate * years);
+    console.log(`Total Interest: $${Interest}`);
+}
+calculateLoanInterest(1000, 0.05, 3); // Expected output: "Total Interest: $150.00"
+calculateLoanInterest(5000, 0.07, 5); // Expected output: "Total Interest: $1750.00"
